e82386d4a2588bcfb96ee79c0ec53e6db2092ff8
Aryan Hansoti <thewebcodrr@gmail.com>
2025-06-29 22:36:06 +0530

    init - created all initial routes for incident

---
+++ backend/src/routes/incident.routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import { deleteIncident, downvoteIncident, getAllIncidents, reportIncident, updateIncidentStatus, updateIncidentVerification, upvoteIncident } from "../controllers/incident.controllers";
+import verifyJWT from "../middlewares/auth.middlewares.js";
+
+const router = express.Router();
+
+router.route("/").get(verifyJWT, getAllIncidents);
+router.route("/report").post(verifyJWT, reportIncident);
+router.route("/:incidentId/status").patch(verifyJWT, updateIncidentStatus);
+router.route("/:incidentId").delete(verifyJWT, deleteIncident)
+router.route("/:incidentId/verification").patch(verifyJWT, updateIncidentVerification);
+router.route("/:incidentId/upvote").patch(verifyJWT, upvoteIncident);
+router.route("/:incidentId/downvote").patch(verifyJWT, downvoteIncident);
+
+export default router;
