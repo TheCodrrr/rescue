a7c5ee6647e239af1051b3b47d323c6345ac09e8
Aryan Hansoti <thewebcodrr@gmail.com>
2025-07-01 18:56:03 +0530

    upd - mounted all the 7 routes for each schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,38 @@
-import express from "express"
-import cookieParser from "cookie-parser"
-import cors from "cors"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-}))
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({ extended: true, limit: "16kb" }))
-app.use(express.static("public"))
-app.use(cookieParser())
+}));
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
-import userRouter from "./routes/user.routes.js"
-import { errorHandler } from "./middlewares/error.middlewares.js";
+
+import userRouter from "./routes/user.routes.js";
+import complaintRouter from "./routes/complaint.routes.js";
+import incidentRouter from "./routes/incident.routes.js";
+import escalationRouter from "./routes/escalation.routes.js";
+import categoryRouter from "./routes/category.routes.js";
+import departmentRouter from "./routes/department.routes.js";
+import feedbackRouter from "./routes/feedback.routes.js";
+
 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/complaints", complaintRouter);
+app.use("/api/v1/incidents", incidentRouter);
+app.use("/api/v1/escalations", escalationRouter);
+app.use("/api/v1/categories", categoryRouter);
+app.use("/api/v1/departments", departmentRouter);
+app.use("/api/v1/feedbacks", feedbackRouter);
 
-// optional but better to use
-app.use(errorHandler)
+
+import { errorHandler } from "./middlewares/error.middlewares.js";
+app.use(errorHandler);
+
 export { app };
