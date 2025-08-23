fb0525df6038b3db0d753e3feb3ddfd86cbce9b2
Aryan Hansoti <thewebcodrr@gmail.com>
2025-08-23 22:54:12 +0530

    feat - added the api code to fetch train info

--- backend/src/services/rail.service.js
+++ backend/src/services/rail.service.js
@@ -1,4 +1,71 @@
 import { pool } from "../config/postgres.js";
+import Prettify from "../utils/prettify.js";
+
+const prettify = new Prettify();
+
+export const ensureTrainExists = async (train_number) => {
+    const existingTrain = await getTrainByNumber(train_number);
+    if (existingTrain) {
+        console.log("Train already exists:", existingTrain);
+        return true;
+    }
+
+    const URL_Train = `https://erail.in/rail/getTrains.aspx?TrainNo=${train_number}&DataSource=0&Language=0&Cache=true`;
+    let train;
+    try {
+        const response = await fetch(URL_Train);
+        const data = await response.text();
+        const json = prettify.CheckTrain(data);
+        train = json.data;
+        const coaches = json.data.coaches;
+        console.log("This is the train data from the api: ", json);
+        for (const [coachType, seats] of Object.entries(coaches)) {
+            console.log("Coach type:", coachType);
+            seats.forEach(seat => {
+                console.log(seat);
+            });
+        }
+        console.log("This is the train data from the api: ", json.data.coaches);
+    } catch (e) {
+        console.error("Error fetching train data:", e);
+    }
+
+    const newTrain = await addTrain({
+        train_number,
+        train_name: train.train_name || "Unknown Train",
+        train_type: train.type || "Unknown train",
+        operator_zone: "Unknown zone",
+        avg_speed: train.average_speed || 0,
+        total_distance: train.distance_from_to || 0,
+
+        // "0" = monday
+        frequency: {
+            0: train.running_days[0] == "1",
+            1: train.running_days[1] == "1",
+            2: train.running_days[2] == "1",
+            3: train.running_days[3] == "1",
+            4: train.running_days[4] == "1",
+            5: train.running_days[5] == "1",
+            6: train.running_days[6] == "1",
+        },
+        coaches: {},
+        routes: {
+            from_station: {
+                name: train.from_stn_name || "Unknown Station",
+                code: train.from_stn_code || "Unknown Code",
+                time: train.from_time || "Unknown Time"
+            },
+            to_station: {
+                name: train.to_stn_name || "Unknown Station",
+                code: train.to_stn_code || "Unknown Code",
+                time: train.to_time || "Unknown Time"
+            }
+        },
+        ticket_prices: {}
+    })
+
+    return newTrain ? true : false;
+};
 
 export const initRailSchema = async () => {
   try {
