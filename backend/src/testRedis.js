37d5f2f8b7322ca37dbfe4f4832535cb47ee3eba
Aryan Hansoti <thewebcodrr@gmail.com>
2025-11-07 00:29:32 +0530

    feat - created the redis setup for ignored complaints by a officer

---
+++ backend/src/testRedis.js
@@ -0,0 +1,20 @@
+import { createClient } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = createClient({
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+  },
+  password: process.env.REDIS_PASSWORD,
+});
+
+client.on("error", (err) => console.error("Redis error:", err));
+
+(async () => {
+  await client.connect();
+  console.log("✅ Connected to Redis!");
+  await client.quit();
+})();
---
+++ backend/utils/redisClient.js
@@ -0,0 +1,19 @@
+import { createClient } from "redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const client = createClient({
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+    },
+    password: process.env.REDIS_PASSWORD,
+});
+
+client.on("error", (err) => console.error("Redis error:", err));
+
+await client.connect();
+console.log("Connected to Redis client");
+
+export default client;
