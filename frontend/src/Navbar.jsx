cb74c3c794097ec2d6e4a7f3ea7d3d53702aedbe
Aryan Hansoti <thewebcodrr@gmail.com>
2025-11-06 18:38:21 +0530

    fix - fixed overflowing in navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -58,11 +58,11 @@ const AuthButtons = React.memo(({ isAuthenticated, handleLogin, handleSignup, ha
                     <button 
                         className="auth-btn signout-btn"
                         onClick={handleSignOut}
+                        title="Sign Out"
                     >
                         <svg className="auth-btn-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                         </svg>
-                        Sign Out
                     </button>
                     <button 
                         className="profile-btn"
