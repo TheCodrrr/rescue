39afc9708406330a18c443f7f267386fe01b0f32
Aryan Hansoti <thewebcodrr@gmail.com>
2025-12-12 17:34:45 +0530

    feat - dispatch the init auth on app start

--- application/app/_layout.tsx
+++ application/app/_layout.tsx
@@ -1,16 +1,29 @@
 import { Stack } from "expo-router";
 import { Provider } from 'react-redux';
 import { store } from '../store';
+import { useEffect } from 'react';
+import { initializeAuth } from '../store/slices/authSlice';
 import './globals.css';
+
+function RootLayoutContent() {
+  useEffect(() => {
+    // Initialize auth state from AsyncStorage on app start
+    store.dispatch(initializeAuth());
+  }, []);
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+      }}
+    />
+  );
+}
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
+      <RootLayoutContent />
     </Provider>
   );
 }
