557759bbfa5832c28d858ab0c91929ea0ce6b5a0
Aryan Hansoti <thewebcodrr@gmail.com>
2026-01-04 17:56:56 +0530

    prod - update 2

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,8 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-    origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN2],
+    // origin: process.env.CORS_ORIGIN,
+    origin: "*",
     credentials: true,
 }));
 app.use(express.json({ limit: "16kb" }));
