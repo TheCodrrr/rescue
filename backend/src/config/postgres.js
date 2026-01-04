78fd9789119b63dc064faf20c6f64534f8d6af2d
Aryan Hansoti <thewebcodrr@gmail.com>
2026-01-04 23:02:33 +0530

    prod - update 12

--- backend/src/config/postgres.js
+++ backend/src/config/postgres.js
@@ -1,24 +1,20 @@
 import pg from "pg";
-
 const { Pool } = pg;
 
 export const pool = new Pool({
-    user: process.env.PG_USER || "postgres",
-    host: process.env.PG_HOST || "localhost",
-    database: process.env.PG_DATABASE || "rescue_management",
-    password: process.env.PG_PASSWORD || "",
-    port: process.env.PG_PORT || 5432,
-})
+  connectionString: process.env.DATABASE_URL, // use the full Render URL
+  ssl: { rejectUnauthorized: false }          // required for Render managed DB
+});
 
 pool.connect()
-    .then(client => {
-        // console.log("Connected to postgresql");
-        client.release();
-    })
-    .catch(err => console.error("PostgreSQL connection error: ", err));
+  .then(client => {
+    // console.log("Connected to PostgreSQL");
+    client.release();
+  })
+  .catch(err => console.error("PostgreSQL connection error: ", err));
 
 process.on("SIGINT", async () => {
-    await pool.end();
-    // console.log("PostgreSQL pool closed");
-    process.exit(0);
-})
+  await pool.end();
+  // console.log("PostgreSQL pool closed");
+  process.exit(0);
+});
