05763f4f22e61aa759ba9f4bbef9c48980637347
Aryan Hansoti <thewebcodrr@gmail.com>
2026-01-10 16:47:00 +0530

    style - fixed minor style bugs

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -134,6 +134,10 @@ const Navbar = () => {
         setIsMobileMenuOpen(false);
     }, []);
 
+    const handleLogoClick = useCallback(() => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, []);
+
     // Common navigation links accessible to both roles
     const commonNavLinks = useMemo(() => [
         { name: 'Home', href: '/home', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
@@ -184,12 +188,12 @@ const Navbar = () => {
             <div className="navbar-container">
                 {/* Logo */}
                 <div className="navbar-brand">
-                    <div className="lodge-logo">
+                    <Link to="/home" className="lodge-logo" onClick={handleLogoClick}>
                         <svg className="lodge-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
                         </svg>
                         <span className="lodge-text">Rescue</span>
-                    </div>
+                    </Link>
                 </div>
 
                 {/* Desktop Navigation */}
